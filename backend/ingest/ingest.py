6dcfe3be0c2da0382ec2407642769159801c1fc3
Christian Moscardi <christianmoscardi@gmail.com>
2024-11-13 11:57:21 -0500

    update ingest to include pdf urls

--- backend/ingest/ingest.py
+++ backend/ingest/ingest.py
@@ -10,6 +10,13 @@ import pymupdf as pdf
 This script idempotently loads all pdfs from /data into ES index
 """
 
+# 1940 is 62nd edition
+BASE_YEAR_MAPPING = (1940, 62)
+
+
+BASE_URL = "http://localhost:4001"
+BASE_ORIG_URL = "https://www2.census.gov/library/publications/{published_year}/compendia/statab/{edition}ed/{fname}"
+
 def main():
     es = Elasticsearch(f"http://{os.environ['ES_LOCAL_CONTAINER_NAME']}:9200",
                        api_key=os.environ["ES_LOCAL_API_KEY"])
@@ -21,10 +28,21 @@ def main():
     for fname in glob.glob("/data/*.pdf"):
         basename = os.path.basename(fname)
         print("loading file", basename)
+        year = int(basename.split("-")[0])
+        edition = (year - BASE_YEAR_MAPPING[0]) + BASE_YEAR_MAPPING[1]
+        published_year = year + 1
+
+        url = BASE_URL + f"/{basename}"
+        orig_url = BASE_ORIG_URL.format(published_year=published_year,
+                                        edition=edition,
+                                        fname=basename)
+
         doc = pdf.open(fname)
         for i, page in enumerate(doc):
             to_load = {"title": f"{basename} page {i}",
-                       "contents": page.get_text()}
+                       "contents": page.get_text(),
+                       "url": url,
+                       "orig_url": orig_url}
             response = es.index(index="sad",
                                 body=to_load)
             if response['result'] != 'created':
--- backend/ingest/search-demo.py
+++ backend/ingest/search-demo.py
@@ -3,12 +3,13 @@ import sys
 
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+import json
 
 def main(query):
     es = Elasticsearch("http://localhost:9200",
                        api_key=os.environ["ES_LOCAL_API_KEY"])
 
-    print(f"searching for {query}")
+    #print(f"searching for {query}")
     results = es.search(
         index='sad',
         query={
@@ -19,11 +20,11 @@ def main(query):
             }
         }
     )
-    print(results)
+    #print(dir(results))
+    print(json.dumps(dict(results)))
 
 
 if __name__ == "__main__":
     load_dotenv()
     query = sys.argv[1]
     main(query)
-
