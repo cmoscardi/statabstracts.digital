8b7308d9b1091eb0dba88f9889e8ef516a46745c
Christian Moscardi <christianmoscardi@gmail.com>
2024-11-25 10:09:47 -0500

    hack hack

--- build/build_frontend.sh
+++ build/build_frontend.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_BASE=$(readlink -f $SCRIPT_DIR/..)
 
 cd $PROJECT_BASE/frontend/app/
+docker network create sadnet
 docker compose run frontend bash -c "cd /app; npm run build"
 ls $PROJECT_BASE/frontend/app/build
