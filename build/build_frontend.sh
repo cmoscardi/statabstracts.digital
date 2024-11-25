49d13fda67c9a0322cfe71185568b79015b728eb
Christian Moscardi <christianmoscardi@gmail.com>
2024-11-25 10:13:47 -0500

    silly node_modules

--- build/build_frontend.sh
+++ build/build_frontend.sh
@@ -5,5 +5,5 @@ PROJECT_BASE=$(readlink -f $SCRIPT_DIR/..)
 
 cd $PROJECT_BASE/frontend/app/
 docker network create sadnet
-docker compose run frontend bash -c "cd /app; npm run build"
+docker compose run frontend bash -c "mv /deps/node_modules /app; cd /app; npm run build"
 ls $PROJECT_BASE/frontend/app/build
