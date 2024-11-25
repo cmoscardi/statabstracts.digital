eca8d8c991b0de903516da35ce76bb262d3a729b
Christian Moscardi <christianmoscardi@gmail.com>
2024-11-25 10:07:15 -0500

    separate jobs for frontend and backend

--- build/build_frontend.sh
+++ build/build_frontend.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_BASE=$(readlink -f $SCRIPT_DIR/..)
 
 cd $PROJECT_BASE/frontend/app/
-npm run build
+docker compose run frontend bash -c "cd /app; npm run build"
+ls $PROJECT_BASE/frontend/app/build
