2738ecdad517e9e275b39995eeb84bf28add2252
Steve Chadwick <stevechadwick@Steves-MBP.localdomain>
2024-12-12 21:38:15 -0500

    Fixing favicon issue and hopefully pdf title issue

--- frontend/app/src/utils/formatTitle.js
+++ frontend/app/src/utils/formatTitle.js
@@ -1,5 +1,11 @@
 export default function formatTitle(str, obj=false) {
-  const year = str.split("-")[0];
+  // formats data from title like "1940-08.pdf page 93" or "1940.pdf page 93"
+
+  let pdf = str.split('.')[0]
+  if(pdf.length === 4) {
+    pdf = `${str}-00`
+  }
+  const year = pdf.split("-")[0];
   const part = str.split("-")[1].substring(0, 2);
   const words = str.split(" ");
   const pageNum = parseInt(words[words.length - 1]) + 1;
